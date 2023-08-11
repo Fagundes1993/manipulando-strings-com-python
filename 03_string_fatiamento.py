145cb20326ea35fd8cb0e06db8edc8a69cd39389
Fagundes1993 <mfbl93@gmail.com>
2023-08-10 22:02:03 -0300

    Adição da aula de fatiamento de string

---
+++ 03_string_fatiamento.py
@@ -0,0 +1,9 @@
+nome = "Matheus Fagundes Brito de Lima"
+
+print(nome[0])
+print(nome[:7])
+print(nome[8:])
+print(nome[8:16])
+print(nome[0:8:2])      #Os primeiros 8 caracteres num intervalo de dois em dois
+print(nome[:])
+print(nome[::-1])       #espelhar string
